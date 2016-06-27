81098b3b26b77b583837823ba2c26d4eae807cb7
Tariq Yousef <info@tariq-yousef.com>
2016-06-27 13:47:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ILA_python',
     version="0.0.1",
     packages=find_packages(exclude=["Examples", "Test"]),
-    url='https://github.com/TariqYousef/ILA_python',
+    url='https://github.com/OpenGreekAndLatin/ILA_python',
     license='GNU GPL',
     author='Tariq Yousef',
     author_email='tariq.yousef@dh.uni-leipzig.de',
