9676fae23835621de85bb1a66fe6670fb09ca5b4
Thibault Clerice <leponteineptique@gmail.com>
2016-06-30 15:00:50 +0200

    Various fixes :
    - PEP8 for tests
    - New test commentaries
    - Rename to lower case of tests and example folder
    - Added .gitignore for PyCharm and Python in general
    - Try at new config for coverage

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='iAlignment',
     version="0.0.1",
-    packages=find_packages(exclude=["Examples", "Test"]),
+    packages=find_packages(exclude=["examples", "tests"]),
     url='https://github.com/OpenGreekAndLatin/ILA_python',
     license='GNU GPL',
     author='Tariq Yousef',
     author_email='tariq.yousef@dh.uni-leipzig.de',
     description='Intra Language Alignment tool',
-    test_suite="Test",
+    test_suite="tests",
     zip_safe=False
 )
